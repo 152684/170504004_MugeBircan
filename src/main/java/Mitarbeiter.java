c566a14d98f46b993c9ffdaf5d6d501451ff5cc3
mbirc <mbirc@LAPTOP-9H46095L>
2022-04-23 10:56:41 +0300

    javaFX und fxml Dateien wurden angeschafft und mit dem Projekt verbunden

---
+++ src/main/java/Mitarbeiter.java
@@ -0,0 +1,19 @@
+
+import java.util.Date;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mbirc
+ */
+public class Mitarbeiter extends Arbeiter{
+    
+    public Mitarbeiter(String n, int ID, String anschr, String e, int telefon, Date geburt/*, int persID*/) {
+        super(n, ID, anschr, e, telefon, geburt/*, personalID*/);
+    }
+    
+}
---
+++ src/main/java/ReiseLeiter.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mbirc
+ */
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ReiseLeiter extends Person{
+    
+    private ArrayList<String> touren = new ArrayList();
+    private int reiseAnzahl = 0;
+    
+    public ReiseLeiter(String n, int ID, String anschr, String e, int telefon, Date geburt) {
+        super(n, ID, anschr, e, telefon, geburt);
+    }
+    
+}
